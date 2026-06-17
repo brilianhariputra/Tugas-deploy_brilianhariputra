84b3e641c9b8c8025a6192c7c02535152ae4144e
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-17 14:11:46 +0700

    Fix: Clear bootstrap cache and update ignore rules

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+
+// 1. Arahkan semua permintaan ke file index.php utama Laravel
+require __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -1,15 +1,7 @@
 <?php
-// Paksa tampilkan error untuk debugging
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-// Mencegah error akses file di Vercel
-putenv('CACHE_DRIVER=array');
-putenv('SESSION_DRIVER=array');
+// Paksa Laravel menggunakan sistem memory agar tidak butuh akses tulis
+putenv('APP_STORAGE=/tmp');
 putenv('VIEW_COMPILED_PATH=/tmp');
-
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
@@ -17,10 +9,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
+    $request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
