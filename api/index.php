b2d08ecd06ac01fb415ccef1a834bec79a3b32f4
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-17 13:03:41 +0700

    Fix v2 vercel config

--- api/index.php
+++ api/index.php
@@ -4,3 +4,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $app = require dirname(__DIR__) . '/bootstrap/app.php';
 $kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
 $kernel->handle(\Illuminate\Http\Request::capture())->send();
+<?php
+
+// Arahkan ke file index.php milik Laravel di folder public
+require __DIR__ . '/../public/index.php';
