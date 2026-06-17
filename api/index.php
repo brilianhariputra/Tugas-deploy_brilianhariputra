5b90200e915eff2a42f04bcc010598ba8fef991c
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-17 14:16:13 +0700

    Fix path: update api index require

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
-
-// 1. Arahkan semua permintaan ke file index.php utama Laravel
-require __DIR__ . '/../public/index.php';
+// Menggunakan __DIR__ untuk mencari root project dengan tepat
+// Kita naik 1 tingkat dari /api ke root, lalu masuk ke /public/index.php
+require_once __DIR__ . '/../public/index.php';
