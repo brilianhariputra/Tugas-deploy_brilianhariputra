a35c3e44aed0538b78213a9da205de2eeb10428d
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-17 13:30:09 +0700

    Test: Debugging dengan Test 1

--- api/index.php
+++ api/index.php
@@ -1,8 +1,3 @@
 <?php
-
-// Tampilkan error agar kita tahu jika ada masalah
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-// Panggil file index.php Laravel yang asli
-require __DIR__ . '/../public/index.php';
+echo "TEST 1: API Index berhasil diakses!";
+die();
