f1decc6f0280038c3e9b4fe9c992059f10c485e9
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-17 13:35:54 +0700

    Test 2: Cek Autoloader

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
-echo "TEST 1: API Index berhasil diakses!";
+// Test 2: Cek apakah file autoload Laravel ditemukan
+$autoload = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoload)) {
+    require $autoload;
+    echo "TEST 2: Autoload sukses! Laravel siap dimuat.";
+} else {
+    echo "TEST 2 ERROR: File vendor/autoload.php tidak ditemukan!";
+}
 die();
