5bcaa7e6af02f90faf19a031eda610e3d4d66ac1
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-17 14:24:09 +0700

    Fix: Force index bootstrap files

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
 // Menggunakan __DIR__ untuk mencari root project dengan tepat
 // Kita naik 1 tingkat dari /api ke root, lalu masuk ke /public/index.php
+<?php
+// Pastikan ini memanggil file public/index.php dengan benar
 require_once __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -7,7 +7,8 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+// Ganti baris require_once yang lama dengan ini:
+require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
