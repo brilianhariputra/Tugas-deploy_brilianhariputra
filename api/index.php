ee949a87d2b5a5a61293343837a32c6fbdbc9061
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-19 12:26:54 +0700

    Update konfigurasi vercel dan fix routing

--- api/index.php
+++ api/index.php
@@ -1,9 +1,4 @@
-$content = [System.Text.Encoding]::UTF8.GetBytes('<?php
-define(''LARAVEL_START'', microtime(true));
-require dirname(__DIR__) . ''/vendor/autoload.php'';
-$app = require dirname(__DIR__) . ''/bootstrap/app.php'';
-$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
-$kernel->handle(\Illuminate\Http\Request::capture())->send();')
+<?php
 
-[System.IO.File]::WriteAllBytes("api\index.php", $content)
-Get-Content api\index.php
+// Arahkan request ke file public/index.php milik Laravel
+require __DIR__ . '/../public/index.php';
