caffa0fdbf5bc016bc88a9a0ffb471f189888205
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-19 09:49:35 +0700

    fix: restore correct api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
-<?php
+$content = [System.Text.Encoding]::UTF8.GetBytes('<?php
+define(''LARAVEL_START'', microtime(true));
+require dirname(__DIR__) . ''/vendor/autoload.php'';
+$app = require dirname(__DIR__) . ''/bootstrap/app.php'';
+$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
+$kernel->handle(\Illuminate\Http\Request::capture())->send();')
 
-require __DIR__ . '/../api/index.php';
+[System.IO.File]::WriteAllBytes("api\index.php", $content)
+Get-Content api\index.php
