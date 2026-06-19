346f0c66b3d07a13d07447fc9d8161e7ce961b4a
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-19 18:51:15 +0700

    fix: fix add_status migration

--- database/migrations/2026_06_19_081113_add_status_to_santris_table.php
+++ database/migrations/2026_06_19_081113_add_status_to_santris_table.php
@@ -1,9 +1,24 @@
-public function up(): void
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::table('santris', function (Blueprint $table) {
-        // Cek dulu apakah kolom 'status' belum ada, baru ditambahkan
-        if (!Schema::hasColumn('santris', 'status')) {
-            $table->string('status')->default('Hadir');
-        }
-    });
-}
+    public function up(): void
+    {
+        Schema::table('santris', function (Blueprint \) {
+            if (!Schema::hasColumn('santris', 'status')) {
+                \->string('status')->default('Hadir');
+            }
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('santris', function (Blueprint \) {
+            \->dropColumn('status');
+        });
+    }
+};
